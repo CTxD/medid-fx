afee14d389d0f69068b2f509bee782563a4a40f6
Dennis Juhl Østergaard <dala15@student.aau.dk>
2019-04-26 09:46:52 +0200

    implemented extraction of colored pixels of a pill

--- source/fx/colorx/cx.py
+++ source/fx/colorx/cx.py
@@ -1,5 +1,6 @@
 from .pillcolorpixels import getcolorpixels
-
+from ...utils import colormap
 
 def main():
-    getcolorpixels()
+    res = getcolorpixels('resources/betolvex.jpg')
+    colormap.img_creater(res, 5, 'betolvex_colormap')
