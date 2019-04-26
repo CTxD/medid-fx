fbc46c2dcd7b18f51c7b78c5852171867479e177
Christian Larsen <c@chrl.dk>
2019-04-26 08:57:39 +0200

    minor cleanup

--- source/fx/colorx/cx.py
+++ source/fx/colorx/cx.py
@@ -1,6 +1,2 @@
-from colormath.color_objects import sRGBColor
-from source.fx.colorx.colormap import _map_rgb_color_to_xterm_color
-
 def main():
-    print(_map_rgb_color_to_xterm_color(sRGBColor(66, 134, 244, is_upscaled=True)))
-   
+    pass
